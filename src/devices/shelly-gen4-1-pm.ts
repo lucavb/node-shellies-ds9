b4503febe3c9dd763f96efd2fea3ddb07bb73536
Luca Becker <luca.becker@me.com>
2026-06-09 18:01:45 +0200

    fix: remove unused Cover import in ShellyGen4Mini
    Unblocks cq:eslint in CI after Gen4 device merge.

--- src/devices/shelly-gen4-1-pm.ts
+++ src/devices/shelly-gen4-1-pm.ts
@@ -1,43 +1,33 @@
 import { component, Device, MultiProfileDevice } from './base';
-import {
-  BluetoothLowEnergy,
-  Cloud,
-  Cover,
-  Input,
-  Mqtt,
-  OutboundWebSocket,
-  Script,
-  Switch,
-  WiFi,
-} from '../components';
+import { BluetoothLowEnergy, Cloud, Input, Mqtt, OutboundWebSocket, Script, Switch, WiFi } from '../components';
 
 export class ShellyGen4Mini extends MultiProfileDevice {
-  static readonly model: string = "S4SW-001P8EU";
-  static readonly modelName: string = "Shelly 1PM Gen4 Mini";
+    static readonly model: string = 'S4SW-001P8EU';
+    static readonly modelName: string = 'Shelly 1PM Gen4 Mini';
 
-  @component
-  readonly wifi = new WiFi(this);
+    @component
+    readonly wifi = new WiFi(this);
 
-  @component
-  readonly bluetoothLowEnergy = new BluetoothLowEnergy(this);
+    @component
+    readonly bluetoothLowEnergy = new BluetoothLowEnergy(this);
 
-  @component
-  readonly cloud = new Cloud(this);
+    @component
+    readonly cloud = new Cloud(this);
 
-  @component
-  readonly mqtt = new Mqtt(this);
+    @component
+    readonly mqtt = new Mqtt(this);
 
-  @component
-  readonly outboundWebSocket = new OutboundWebSocket(this);
+    @component
+    readonly outboundWebSocket = new OutboundWebSocket(this);
 
-  @component
-  readonly input = new Input(this, 0);
+    @component
+    readonly input = new Input(this, 0);
 
-  @component
-  readonly switch = new Switch(this, 0);
+    @component
+    readonly switch = new Switch(this, 0);
 
-  @component
-  readonly script = new Script(this);
+    @component
+    readonly script = new Script(this);
 }
 
 Device.registerClass(ShellyGen4Mini);
